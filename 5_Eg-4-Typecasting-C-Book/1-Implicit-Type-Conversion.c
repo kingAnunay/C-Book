55302bf1d12de42be66c68ebbb04322986dc83fd
Anunay <anunaynmit@gmail.com>
2020-11-19 18:24:27 +0000

    Added 5

---
+++ 5_Eg-4-Typecasting-C-Book/1-Implicit-Type-Conversion.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    When the type conversion is performed automatically by the compiler without programmers intervention, such type of conversion is known as implicit type conversion or type promotion.
+  return 0;
+}
